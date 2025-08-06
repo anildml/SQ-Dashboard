86144e6b670378a73dafd064a1e858803af74b74
anil <ianilyurtsever@gmail.com>
2025-08-06 16:41:55 +0300

    removed commented out function

--- src/app/models/interfaces/session.ts
+++ src/app/models/interfaces/session.ts
@@ -5,14 +5,3 @@ export interface Session extends Flow {
   client_name: string;
 }
 
-//export function isSession(event: Event) {
-//  return (
-//    "_id" in event &&
-//    "client_id" in event &&
-//    "starter_node_id" in event &&
-//    "start_time" in event &&
-//    "end_time" in event &&
-//    "events" in event &&
-//    "state_map_history" in event
-//    );
-//}
