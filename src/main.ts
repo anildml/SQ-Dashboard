24d7644726d779e095ef74ff75213d5fbe6166bd
anil <ianilyurtsever@gmail.com>
2025-11-25 15:38:19 +0300

    main component renaming

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app_main/app.config';
-import { App } from './app/app_main/app';
+import { appConfig } from './app/main/app.config';
+import { App } from './app/main/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
