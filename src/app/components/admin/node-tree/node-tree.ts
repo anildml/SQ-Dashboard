9f238eb01a53f39483d365fa06bff2ec82289fb7
anil <ianilyurtsever@gmail.com>
2025-12-10 14:56:00 +0300

    wip - leaderline draw line logic

--- src/app/components/admin/node-tree/node-tree.ts
+++ src/app/components/admin/node-tree/node-tree.ts
@@ -2,7 +2,7 @@ import {
   Component,
   Input,
   OnInit,
-  QueryList, signal,
+  QueryList, signal, viewChild,
   ViewChildren, WritableSignal
 } from '@angular/core';
 import {Node} from '../../../models/interfaces/node';
@@ -36,6 +36,22 @@ export class NodeTreeComponent implements OnInit {
 
   constructor() {
 
+  }
+
+  drawLines(parentNodeID: string, childNodeIDs: string[]) {
+    for (let childNodeID of childNodeIDs) {
+      let parentNode = document.getElementById("node_" + parentNodeID);
+      let childNode = document.getElementById("node_" + childNodeID);
+      let line = new LeaderLine(parentNode, childNode, {
+        hide: true,
+        path: "fluid",
+        startPlug: "square",
+        startSocket: "bottom",
+        endSocket: "top",
+        endPlug: "disc"
+      });
+      line.show("draw", {duration: 200});
+    }
   }
 
   async ngOnInit(): Promise<void> {
@@ -100,7 +116,16 @@ export class NodeTreeComponent implements OnInit {
   }
 
   async expandLayer() {
-    setTimeout(() => this.layers.last.open(), 250); // timeout added for animation
+    await new Promise<void>(resolve => {
+      setTimeout(() => {
+        this.layers.last.open();
+        resolve();
+      }, 250); // timeout added for animation
+    });
+    await firstValueFrom(this.layers.last.afterExpand);
+    let parentNode = this.treePath().at(-2)?.selectedNode?.id!;
+    let childNodes = this.treePath().at(-1)?.nodeList?.map(n => n.id)!;
+    this.drawLines(parentNode, childNodes);
   }
 
   isNewBranch(layerIndex: number, clickedNode: Node): boolean {
@@ -121,7 +146,7 @@ export class NodeTreeComponent implements OnInit {
     let layersToCollapse: MatExpansionPanel[] = [];
     for (let i = layerIndex + 1; i < this.treePath().length; i++) {
       let layer = this.layers.get(i)!;
-      layersCollapsed.push(firstValueFrom(layer.afterCollapse.asObservable()));
+      layersCollapsed.push(firstValueFrom(layer.afterCollapse));
       layersToCollapse.push(layer);
     }
     layersToCollapse.forEach(layer => layer.close());
