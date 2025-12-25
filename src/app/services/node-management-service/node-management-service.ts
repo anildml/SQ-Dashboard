9d6a9e2aa4255be35deff1a886bdf569e7215bec
anil <ianilyurtsever@gmail.com>
2025-12-25 16:28:48 +0300

    node management service simplification

--- src/app/services/node-management-service/node-management-service.ts
+++ src/app/services/node-management-service/node-management-service.ts
@@ -1,43 +1,15 @@
-import {EventEmitter, inject, Injectable, signal, WritableSignal} from '@angular/core';
-import {Operation, UpdateSchema} from "../../models/interfaces/operation";
-import {NodeTreeService} from '../node-tree-service/node-tree-service';
+import {computed, EventEmitter, Injectable, Signal, signal, WritableSignal} from '@angular/core';
+import {Operation} from "../../models/interfaces/operation";
 
 @Injectable({
   providedIn: 'root',
 })
 export class NodeManagementService {
 
-  defineUpdateSchemaMode: WritableSignal<boolean> = signal(false);
-
+  operationToEdit: WritableSignal<Operation | null> = signal(null);
+  operationEditMode: Signal<boolean> = computed(() => this.operationToEdit() != null);
   addStateToOperationUpdateSchemaEventEmitter: EventEmitter<any> = new EventEmitter<any>();
-  nodeTreeService: NodeTreeService = inject(NodeTreeService);
   
-  constructor() {
-    this.addStateToOperationUpdateSchemaEventEmitter.subscribe(data => {
-      this.switchStateOnOperationUpdateSchema(data);
-    });
-  }
-
-  switchStateOnOperationUpdateSchema(stateData: any) {
-    let operation = {} as Operation;
-    let existingUpdateSchema = operation.update_schema_list.find(us => us.node_id === stateData.nodeId);
-    let updateSchema: UpdateSchema = existingUpdateSchema ? existingUpdateSchema : {
-      node_id: stateData.nodeId,
-      node_name: stateData.nodeName,
-      effected_states: []
-    };
-    if (!existingUpdateSchema) {
-      operation!.update_schema_list.push(updateSchema);
-    }
-    let effectedState = updateSchema.effected_states?.find(es => es == stateData.state);
-    if (!effectedState) {
-      updateSchema.effected_states?.push(stateData.state);
-    } else {
-      updateSchema.effected_states = updateSchema.effected_states?.filter(es => es != stateData.state);
-      if (updateSchema.effected_states?.length == 0) {
-        operation!.update_schema_list = operation!.update_schema_list.filter(us => us.node_id != updateSchema.node_id);
-      }
-    }
-  }
+  constructor() {}
 
 }
