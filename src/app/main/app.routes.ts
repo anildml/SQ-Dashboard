7056a6b3bfc81f6473b9c01b4de7ad33df7f9c2d
anil <ianilyurtsever@gmail.com>
2026-01-23 15:53:02 +0300

    router template

--- src/app/main/app.routes.ts
+++ src/app/main/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {SessionComponent} from '../components/dashboard/session/session';
+import {AdminComponent} from '../components/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: AdminComponent
+  },
+  {
+    path: "**",
+    component: AdminComponent
+  },
+  {
+    path: "admin",
+    component: AdminComponent
+  },
+  {
+    path: "dashboard",
+    component: SessionComponent
+  }
+];
