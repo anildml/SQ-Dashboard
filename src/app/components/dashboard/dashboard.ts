1b0c9de6717e3d66cbdccec5f64febc5467df759
anil <ianilyurtsever@gmail.com>
2026-01-28 14:06:10 +0300

    dashboard component search structure

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, model} from '@angular/core';
+import {MatFormField, MatInputModule, MatLabel} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {FormsModule} from '@angular/forms';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    FormsModule,
+    MatInputModule,
+    MatIcon
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class DashboardComponent {
+
+  searchTypes = ["ID", "Name"];
+  searchType = model(this.searchTypes[0]);
+  searchValue = model("");
+
+  searchClient() {
+
+  }
 
 }
